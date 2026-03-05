e68e82943517992bb286be53c7249e92adce3741
Alypiang <alypiang22@gmail.com>
2026-03-04 22:28:23 -0500

    i hope this works

--- app/teacher/classroom/[id]/page.tsx
+++ app/teacher/classroom/[id]/page.tsx
@@ -316,6 +316,8 @@ export default function TeacherClassroomPage() {
             </div>
           </div>
 
+        </div>
+
         <div className="flex-1 flex">
           <div className="flex-1 p-6 space-y-6">
             {/* Tab Navigation */}
